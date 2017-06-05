0b343767e43074680821c900d49c02cb843c10ac
Igor Molina <canutito1@hotmail.com>
2017-06-05 21:15:55 +0200

    Added changes to src/kalman_filter.cpp Defined linear Kalman Filter equations (constant velocity) for Lidar Updates and non-linear Extended Kalman Filter equations for updating Radar Measurements.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -22,6 +22,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,6 +32,18 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -36,4 +51,31 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  float px = x_(0);
+  float py = x_(1);
+  float c1 = px*px+py*py;
+
+  //check division by zero
+  if(fabs(c1) < 0.0001){
+
+    c1 = 0.0001;
+  }
+
+  VectorXd z_pred = VectorXd(3);
+  z_pred(0) = sqrt(c1);
+  z_pred(1) = atan2(py, px);
+  z_pred(2) = (px*x_(2) + py*x_(3))/z_pred(0);
+
+  VectorXd y = z - z_pred;
+  y(1) = fmod(y(1), M_PI);
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
